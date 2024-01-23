f61e5dbd024d1b503e66e87ebca02f9cf61ab9d6
Vilma Öhrman <vilma.ohrman@hyperisland.se>
2024-01-23 14:37:09 +0100

    bouncing ball

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+let x = 0;
+let speed = 3;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  stroke(225);
+  strokeWeight(4);
+  noFill();
+  ellipse(x, 200, 100, 100);
+
+  if (x > width) {
+    speed = -3;
+  }
+
+  if (x < 0) {
+    speed = +3;
+  }
+
+  x = x + speed;
 }
